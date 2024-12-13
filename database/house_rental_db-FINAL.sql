764906eebaea72905479afbab2b5ce2a08a96c14
Willianne Donayre <donayre.williannelorraine@ue.edu.ph>
2024-12-13 17:59:00 +0800

    added new database

---
+++ database/house_rental_db-FINAL.sql
@@ -0,0 +1,274 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 13, 2024 at 10:57 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `house_rental_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int(30) NOT NULL,
+  `name` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`) VALUES
+(5, 'Studio'),
+(6, 'Bedroom Unit'),
+(7, 'Loft Type'),
+(8, 'Flat-Level'),
+(9, 'Bi-Level'),
+(10, 'Penthouse');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `houses`
+--
+
+CREATE TABLE `houses` (
+  `id` int(30) NOT NULL,
+  `house_no` varchar(50) NOT NULL,
+  `category_id` int(30) NOT NULL,
+  `description` text NOT NULL,
+  `price` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `houses`
+--
+
+INSERT INTO `houses` (`id`, `house_no`, `category_id`, `description`, `price`) VALUES
+(3, '301', 7, 'Mezzanine type with living room, kitchen, and bathroom below and bed above', 10000),
+(4, '201', 6, '1 bedroom with complete basic areas with additional working area', 7500),
+(5, '501', 9, '2 levels house-like unit ; First floor - living room, kitchen, dining area, work area, bathroom ; Second floor - bedroom, bathroom 2', 15000),
+(6, '401', 8, '1 level unit with bedroom, living room, bathroom, kitchen, and dining area on same level', 12000),
+(7, '601', 10, 'Large floor area with complete bedroom, living room, bathroom, dining area, kitchen, work area, walk-in closet, balcony, and private pool', 30000),
+(8, '101', 5, 'Affordable unit with complete basic necessities', 5500),
+(9, '102', 5, 'Affordable unit with complete basic necessities', 5750);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payments`
+--
+
+CREATE TABLE `payments` (
+  `id` int(30) NOT NULL,
+  `tenant_id` int(30) NOT NULL,
+  `amount` float NOT NULL,
+  `invoice` varchar(50) NOT NULL,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `payments`
+--
+
+INSERT INTO `payments` (`id`, `tenant_id`, `amount`, `invoice`, `date_created`) VALUES
+(4, 3, 10000, '010724-1', '2024-01-07 20:10:33'),
+(5, 3, 10000, '020724-1', '2024-02-07 20:12:50'),
+(6, 3, 10000, '030724-1', '2024-03-07 20:13:04'),
+(7, 3, 10000, '040724-1', '2024-04-07 20:13:17'),
+(8, 3, 10000, '050724-1', '2024-05-07 20:14:44'),
+(9, 3, 10000, '060724-1', '2024-06-07 20:14:55'),
+(10, 3, 10000, '070724-1', '2024-07-07 20:15:32'),
+(11, 3, 10000, '080724-1', '2024-08-07 20:15:44'),
+(12, 3, 10000, '090724-1', '2024-09-07 20:15:56'),
+(13, 3, 10000, '100724-1', '2024-10-07 20:16:10'),
+(14, 3, 10000, '110724-1', '2024-11-07 20:16:20'),
+(15, 3, 10000, '120724-1', '2024-12-07 20:16:32'),
+(16, 4, 15000, '022124-1', '2024-02-21 16:56:41'),
+(17, 4, 15000, '031924-1', '2024-03-19 17:01:44'),
+(18, 4, 15000, '041924-1', '2024-04-19 17:02:23'),
+(19, 4, 10000, '051224-1', '2024-05-12 17:03:17'),
+(20, 4, 5000, '052824-1', '2024-05-28 17:03:41'),
+(21, 4, 15000, '061624-1', '2024-06-16 17:07:20'),
+(22, 4, 15000, '080124-1', '2024-08-01 17:08:37'),
+(23, 4, 15000, '083124-1', '2024-08-31 17:08:54'),
+(24, 4, 15000, '092224-1', '2024-09-22 17:09:29'),
+(25, 5, 11500, '050424-1', '2024-05-04 17:15:14'),
+(26, 5, 5750, '060824-1', '2024-06-08 17:15:32'),
+(27, 5, 3250, '070324-1', '2024-07-03 17:16:14'),
+(28, 5, 2500, '071624-1', '2024-07-16 17:17:56');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `system_settings`
+--
+
+CREATE TABLE `system_settings` (
+  `id` int(30) NOT NULL,
+  `name` text NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `contact` varchar(20) NOT NULL,
+  `cover_img` text NOT NULL,
+  `about_content` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `system_settings`
+--
+
+INSERT INTO `system_settings` (`id`, `name`, `email`, `contact`, `cover_img`, `about_content`) VALUES
+(1, 'JJY\'S CONDOMINIUM', 'info@sample.comm', '+6948 8542 623', '1603344720_1602738120_pngtree-purple-hd-business-banner-image_5493.jpg', '&lt;p style=&quot;text-align: center; background: transparent; position: relative;&quot;&gt;&lt;span style=&quot;color: rgb(0, 0, 0); font-family: &amp;quot;Open Sans&amp;quot;, Arial, sans-serif; font-weight: 400; text-align: justify;&quot;&gt;&amp;nbsp;is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&rsquo;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.&lt;/span&gt;&lt;br&gt;&lt;/p&gt;&lt;p style=&quot;text-align: center; background: transparent; position: relative;&quot;&gt;&lt;br&gt;&lt;/p&gt;&lt;p style=&quot;text-align: center; background: transparent; position: relative;&quot;&gt;&lt;br&gt;&lt;/p&gt;&lt;p&gt;&lt;/p&gt;');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tenants`
+--
+
+CREATE TABLE `tenants` (
+  `id` int(30) NOT NULL,
+  `firstname` varchar(100) NOT NULL,
+  `middlename` varchar(100) NOT NULL,
+  `lastname` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `contact` varchar(50) NOT NULL,
+  `house_id` int(30) NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT 1 COMMENT '1 = active, 0= inactive',
+  `date_in` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tenants`
+--
+
+INSERT INTO `tenants` (`id`, `firstname`, `middlename`, `lastname`, `email`, `contact`, `house_id`, `status`, `date_in`) VALUES
+(2, 'John', 'C', 'Smith', 'jsmith@sample.com', '+18456-5455-55', 1, 0, '2020-07-02'),
+(3, 'Willianne Lorraine', 'M.', 'Donayre', 'annedonayre254@gmail.com', '09350167343', 3, 1, '2023-12-08'),
+(4, 'Wallen Paul', 'D.', 'Monzon', 'monzon.wallenpaul@ue.edu.ph', '09185857675', 5, 1, '2024-01-17'),
+(5, 'Ernest Gerald', 'B.', 'Dimaano', 'dimaano.ernestgerald@ue.edu.ph', '09498151746', 9, 1, '2024-03-13');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(30) NOT NULL,
+  `name` text NOT NULL,
+  `username` varchar(200) NOT NULL,
+  `password` text NOT NULL,
+  `type` tinyint(1) NOT NULL DEFAULT 2 COMMENT '1=Admin,2=Staff'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `username`, `password`, `type`) VALUES
+(1, 'Administrator', 'admin', '0192023a7bbd73250516f069df18b500', 1),
+(12, 'Administrator 2', 'admin2', '1a145a23d6e47aadfe2063f1f951e691', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `houses`
+--
+ALTER TABLE `houses`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `payments`
+--
+ALTER TABLE `payments`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `system_settings`
+--
+ALTER TABLE `system_settings`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tenants`
+--
+ALTER TABLE `tenants`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `houses`
+--
+ALTER TABLE `houses`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `payments`
+--
+ALTER TABLE `payments`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+
+--
+-- AUTO_INCREMENT for table `system_settings`
+--
+ALTER TABLE `system_settings`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tenants`
+--
+ALTER TABLE `tenants`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
